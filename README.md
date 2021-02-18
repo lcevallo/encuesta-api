5ec3c32c555ee53e86f8fe2497c3f9b67b0f3366
Luis Alberto Cevallos Cavero <lcevallo@gmail.com>
2021-02-18 09:38:57 -0500

    Estudiantes Fotos
    Se agrego la funcion para descargar las fotos de la base de ekudemic de la tabla estudiantes_fotos

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # encuesta-api
- Aprendiendo con Python Flask Api con Mysql 
+Aprendiendo con Python Flask Api con Mysql 
 
 https://stackoverflow.com/questions/5010042/mysql-get-column-name-or-alias-from-query
+
+https://vslive.com/Blogs/News-and-Tips/2015/04/5-VS-Keyboard-Shortcuts.aspx#:~:text=Comment%20Code%20Block%20Ctrl%2BK,C%2FCtrl%2BK%2BU&text=If%20you%20select%20a%20block,U%20will%20uncomment%20the%20code.
+
+
+python test-columns.py > output.txt
---
+++ test-columns.py
@@ -0,0 +1,38 @@
+import myconnutils
+
+connection = myconnutils.getConnectionEkudemic()
+cursor = connection.cursor()
+
+query = "SELECT * FROM public.estudiante_foto"
+# query = "SELECT *  FROM information_schema.columns WHERE table_schema = 'public' AND table_name   = 'estudiante_foto' "
+
+cursor.execute(query)
+records = cursor.fetchall()
+
+
+nombres = []
+tipo = []
+
+columns_sp = []
+
+colums_update = []
+
+str2 = " " 
+
+
+for record in records:
+    open("fotos/"+record[2]+'.jpeg', 'wb') .write(record[3])
+    # print(record)
+    # nombres.append(record[3])
+    # tipo.append(record[7])
+    # # columns_sp.append("DECLARE v"+ str(record[3]).capitalize()+' '+ record['data_type'] + ';\n')
+    # columns_sp.append("SET v"+ str(record[3]).capitalize()+"=  JSON_EXTRACT(pParametroJson, CONCAT('$[', vIndex, ']."+str(record[3])+"'));\n")
+    # colums_update.append(" "+ str(record[3])+" = v"+ str(record[3]).capitalize()+",\n")
+    
+    
+
+print(nombres)
+print(tipo)
+print(str2.join(colums_update))
+connection.close()
+
